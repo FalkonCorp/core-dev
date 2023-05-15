980deb6085e3de4c1a40621abff2a517f6f6d053
F4lk0n <otaviosoria@gmail.com>
2023-05-15 09:14:37 -0300

    Test Types

--- tests/VDDTest.php
+++ tests/VDDTest.php
@@ -5,8 +5,38 @@ final class VDDTest extends TestCase
 {
     public function testCallerFile(): void
     {
-        $this->expectOutputRegex('/(VDD)/');
+        $this->expectOutputRegex('/(\/VDDTest\<)/');
         VD('XXX');
+    }
+    
+    public function testCallerFilePath(): void
+    {
+        $this->expectOutputRegex('/(tests\/VDDTest\<)/');
+        VD('XXX');
+    }
+    
+    public function testTypeString(): void
+    {
+        $this->expectOutputRegex('/(\>string\<)/');
+        VD('XXX');
+    }
+    
+    public function testTypeInteger(): void
+    {
+        $this->expectOutputRegex('/(\>integer\<)/');
+        VD(1);
+    }
+    
+    public function testTypeArray(): void
+    {
+        $this->expectOutputRegex('/(\>array\<)/');
+        VD([]);
+    }
+    
+    public function testTypeObject(): void
+    {
+        $this->expectOutputRegex('/(\>object\<)/');
+        VD((object)[]);
     }
     
     
